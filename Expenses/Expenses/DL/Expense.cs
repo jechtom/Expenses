5da8df8fbde9d8eef5c8e8fe48a5181e071469f9
Tomas Jecha <tomas@jecha.net>
2014-03-01 18:15:51 +0100

    - added DL context

---
+++ Expenses/Expenses/DL/Expense.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Expenses.DL
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+
+        public virtual ICollection<ExpenseItem> Items { get; set; }
+
+        public string Name { get; set; }
+
+        public 
+    }
+}
---
+++ Expenses/Expenses/DL/ExpenseItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+namespace Expenses.DL
+{
+    public class ExpenseItem
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public Expense Expense { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+    }
+}
